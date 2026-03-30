08fd1584e822330a49b18f727ccb7a2bf63fcfec
Braeden Rodgers <braedenrodgerss@gmail.com>
2026-03-30 12:24:08 -0400

    Update colors.h

--- colors.h
+++ colors.h
@@ -1,7 +1,7 @@
 /**
  * colors.h 
  * 
- * Header file that define a set of constant strings representing ANSI escape
+ * Header file that defines a set of constant strings representing ANSI escape
  * codes for coloring text in the terminal
  * 
  */
@@ -10,57 +10,56 @@
 #define COLORS_H
 
 #include <string>
-using namespace std;
 
 // Text Formatting Styles
-const string RESET      = "\e[0m";
-const string BOLD       = "\e[1m";
-const string ITALIC     = "\e[3m";
-const string UNDERLINE  = "\e[4m";
-const string BLINK      = "\e[5m";          	
+const std::string RESET      = "\e[0m";
+const std::string BOLD       = "\e[1m";
+const std::string ITALIC     = "\e[3m";
+const std::string UNDERLINE  = "\e[4m";
+const std::string BLINK      = "\e[5m";          	
 
 // Regular Colors
-const string BLACK      = "\e[30m";
-const string RED        = "\e[31m";
-const string GREEN      = "\e[32m";
-const string YELLOW     = "\e[33m";
-const string BLUE       = "\e[34m";
-const string PURPLE     = "\e[35m";
-const string CYAN       = "\e[36m";
-const string WHITE      = "\e[37m";
+const std::string BLACK      = "\e[30m";
+const std::string RED        = "\e[31m";
+const std::string GREEN      = "\e[32m";
+const std::string YELLOW     = "\e[33m";
+const std::string BLUE       = "\e[34m";
+const std::string PURPLE     = "\e[35m";
+const std::string CYAN       = "\e[36m";
+const std::string WHITE      = "\e[37m";
 
 // Background Colors
-const string BG_BLACK   = "\e[40m";
-const string BG_RED     = "\e[41m";
-const string BG_GREEN   = "\e[42m";
-const string BG_YELLOW  = "\e[43m";
-const string BG_BLUE    = "\e[44m";
-const string BG_PURPLE  = "\e[45m";
-const string BG_CYAN    = "\e[46m";
-const string BG_WHITE   = "\e[47m";
+const std::string BG_BLACK   = "\e[40m";
+const std::string BG_RED     = "\e[41m";
+const std::string BG_GREEN   = "\e[42m";
+const std::string BG_YELLOW  = "\e[43m";
+const std::string BG_BLUE    = "\e[44m";
+const std::string BG_PURPLE  = "\e[45m";
+const std::string BG_CYAN    = "\e[46m";
+const std::string BG_WHITE   = "\e[47m";
 
 // High Intensity Colors
-const string H_BLACK    = "\e[90m";
-const string H_RED      = "\e[91m";
-const string H_GREEN    = "\e[92m";
-const string H_YELLOW   = "\e[93m";
-const string H_BLUE     = "\e[94m";
-const string H_PURPLE   = "\e[95m";
-const string H_CYAN     = "\e[96m";
-const string H_WHITE    = "\e[97m";
+const std::string H_BLACK    = "\e[90m";
+const std::string H_RED      = "\e[91m";
+const std::string H_GREEN    = "\e[92m";
+const std::string H_YELLOW   = "\e[93m";
+const std::string H_BLUE     = "\e[94m";
+const std::string H_PURPLE   = "\e[95m";
+const std::string H_CYAN     = "\e[96m";
+const std::string H_WHITE    = "\e[97m";
 
 // High Intensity Background Colors
-const string HBG_BLACK  = "\e[100m";
-const string HBG_RED    = "\e[101m";
-const string HBG_GREEN  = "\e[102m";
-const string HBG_YELLOW = "\e[103m";
-const string HBG_BLUE   = "\e[104m";
-const string HBG_PURPLE = "\e[105m";
-const string HBG_CYAN   = "\e[106m";
-const string HBG_WHITE  = "\e[107m";
+const std::string HBG_BLACK  = "\e[100m";
+const std::string HBG_RED    = "\e[101m";
+const std::string HBG_GREEN  = "\e[102m";
+const std::string HBG_YELLOW = "\e[103m";
+const std::string HBG_BLUE   = "\e[104m";
+const std::string HBG_PURPLE = "\e[105m";
+const std::string HBG_CYAN   = "\e[106m";
+const std::string HBG_WHITE  = "\e[107m";
 
 // Shortcuts 
-const string ERROR      = "\e[1;41;37m\a";  // Bold white text with red background
-const string MENU       = "\e[44;37m";      // Regular white text with blue background
+const std::string ERROR      = "\e[1;41;37m\a";  // Bold white text with red background
+const std::string MENU       = "\e[44;37m";      // Regular white text with blue background
 
 #endif  // COLORS_H
